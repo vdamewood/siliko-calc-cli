992118c5826b3f69c9efc5965bdce4b604f67a24
Vincent Damewood <git@vdamewood.com>
2025-12-01 08:39:56 -0800

    Add code to use pkg-config to find readline

--- cmake/FindReadline.cmake
+++ cmake/FindReadline.cmake
@@ -1,5 +1,5 @@
 # FindReadline.cmake: Find Readline
-# Copyright 2020 Vincent Damewood
+# Copyright 2020, 2025 Vincent Damewood
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,27 +14,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-message(STATUS "Finding Readline")
+message(CHECK_START "Finding Readline")
+
+if(TARGET Readline::Readline)
+	message(CHECK_PASS "Found existing CMake target, skipping.")
+	set(PARENT_SCOPE Readline_FOUND 1)
+	return()
+endif()
+
+find_package(PkgConfig)
+
+if(PkgConfig_FOUND)
+	pkg_check_modules(Readline readline IMPORTED_TARGET)
+	if(TARGET PkgConfig::Readline AND NOT TARGET Readline::Readline)
+		add_library(Readline::Readline ALIAS PkgConfig::Readline)
+		message(CHECK_PASS "Found readline.pc with pkg-config")
+		set(PARENT_SCOPE Readline_FOUND 1)
+		return()
+	endif()
+endif()
 
 find_library(Readline_LIBRARY readline)
-if(Readline_LIBRARY)
-	message(STATUS "  Readline library found at ${Readline_LIBRARY}")
-else()
-	message(STATUS "  Readline library not found.")
-endif()
-
 find_path(Readline_INCLUDE_DIR NAMES readline/readline.h)
-if(Readline_INCLUDE_DIR)
-	message(STATUS "  Readline includes found at ${Readline_INCLUDE_DIR}")
-else()
-	message(STATUS "  Readline includes not found.")
-endif()
-
 if(Readline_LIBRARY AND Readline_INCLUDE_DIR)
 	add_library(Readline::Readline UNKNOWN IMPORTED GLOBAL)
 	set_property(TARGET Readline::Readline PROPERTY IMPORTED_LOCATION "${Readline_LIBRARY}")
 	target_include_directories(Readline::Readline INTERFACE "${Readline_INCLUDE_DIR}")
-	set(Readline_FOUND 1)
-else()
-	set(Readline_FOUND 0)
+	message(CHECK_PASS "Found ${Readline_LIBRARY} and ${Readline_INCLUDE_DIR}/readline/readline.h")
+	set(PARENT_SCOPE Readline_FOUND 1)
+	return()
 endif()
+
+message(CHECK_FAIL "No suitable library found")
+set(PARENT_SCOPE Readline_FOUND 0)
